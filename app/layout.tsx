44bf326b325ed9cd3a0a0d8d91ccb3b1a3e0941a
morganrlb <morganrealbuto@gmail.com>
2025-08-15 23:21:39 +0200

    feat: Correggi il titolo nel metadata da "Easy-Gym" a "Easy Gym" per coerenza visiva

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Easy-Gym",
+  title: "Easy Gym",
   description: "La tua guida al Bodybuilding | Schede personalizzate",
   keywords: ["Bodybuilding", "Fitness", "Allenamento", "Schede Personalizzate", "Corso Bodybuilding"],
 };
