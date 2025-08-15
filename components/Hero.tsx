5115a6798e4a7fb4eb00242800c58a2f2d16fb7a
morganrlb <morganrealbuto@gmail.com>
2025-08-15 20:34:53 +0200

    feat: Aggiorna il titolo e la descrizione nel layout, migliora la coerenza visiva del componente Hero e aggiorna la favicon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
          <section className="relative text-center mt-30 mb-40">
             <div className="container mx-auto px-4">
-               <h1 className="text-4xl md:text-5xl font-bold mb-6">
+               <h1 className="text-[2.5rem] md:text-5xl font-bold mb-6">
                      La Tua Guida <FlipWords words={["al Bodybuilding", "Facile"]} />
                </h1>
                <p className="text-lg md:text-xl text-neutral-300 mb-8 max-w-3xl mx-auto">
